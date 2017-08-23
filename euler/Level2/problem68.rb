314465c54f8f523cf384a4ac1e6d19fd788a5820
Michael Wynholds <mike@carbonfive.com>
2017-08-22 17:23:50 -0700

    Problem 68

---
+++ euler/Level2/problem68.rb
@@ -0,0 +1,78 @@
+require '../helper'
+
+=begin
+
+      g
+       \
+      __a__    f
+    /      \ /
+   b        e
+ /  \      /
+h    c -- d -- j
+      \
+       i
+
+=end
+
+class Problem68
+
+  def initialize
+  end
+
+  def solve_for(sides, sum, block = nil)
+    block ||= Proc.new { true }
+    range = 1..sides*2
+    inners = range.to_a.permutation(sides)
+    inners.map do |inner|
+      available = range.to_a - inner
+      outer = inner.map.with_index do |x, i|
+        y = inner[(i+1)%sides]
+        available.delete(sum - x - y)
+      end
+      outer.include?(nil) ? nil : Solution.new(inner, outer)
+    end.compact.uniq { |s| s.numeric }.select(&block)
+  end
+
+  def max_for(sides, block = nil)
+    range = 1..sides*2
+    min = range.first(3).sum
+    max = range.last(3).sum
+    (min..max).map { |n| solve_for(sides, n, block) }.flatten.map(&:numeric).max
+  end
+
+  def solve
+    digit16 = Proc.new { |s| s.numeric.to_s.length == 16 }
+    puts max_for(5, digit16)
+  end
+
+  def test
+    assert [ 423531612, 432621513 ], solve_for(3, 9).map(&:numeric).sort
+    assert [ 235451613, 253631415 ], solve_for(3, 10).map(&:numeric).sort
+    assert 432621513, max_for(3)
+    assert 253631415, max_for(3, Proc.new { |s| s.numeric.to_s[0] == '2' })
+  end
+end
+
+class Solution
+  def initialize(inner, outer)
+    @inner = inner
+    @outer = outer
+  end
+
+  def numeric
+    index = @outer.index @outer.min
+    router = @outer.rotate index
+    rinner = @inner.rotate index
+    router.map.with_index do |o, i|
+      [ o, rinner[i], rinner[(i+1)%rinner.length] ]
+    end.flatten.join('').to_i
+  end
+
+  def to_s
+
+  end
+end
+
+Problem68.new.test
+puts
+Problem68.new.solve
