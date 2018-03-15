ba3e71dc028b8495b6d8acb584b7ecf6f426fecf
Michael Wynholds <mike@carbonfive.com>
2018-03-15 15:34:21 -0700

    Problem 6

--- cryptopals/crypto.rb
+++ cryptopals/crypto.rb
@@ -11,8 +11,12 @@ class Crypto
     Base64.strict_encode64 bin
   end
 
+  def self.base642bin(base64)
+    Base64.decode64 base64
+  end
+
   def self.single_chars
-    (?!..?~)
+    (?!..?~).to_a + [' ']
   end
 
   def self.xor(bin, key)
@@ -26,16 +30,27 @@ class Crypto
     b1.zip(b2).map { |a,b| a^b }.pack "U*"
   end
 
+  def self.english_letters
+    { 'a' => 8.167, 'b' => 1.492, 'c' => 2.782, 'd' => 4.253, 'e' => 12.702, 'f' => 2.228, 'g' => 2.015, 'h' => 6.094, 'i' => 6.966,
+      'j' => 0.153, 'k' => 0.772, 'l' => 4.025, 'm' => 2.406, 'n' => 6.749,  'o' => 7.507, 'p' => 1.929, 'q' => 0.095, 'r' => 5.987,
+      's' => 6.327, 't' => 9.056, 'u' => 2.758, 'v' => 0.978, 'w' => 2.360,  'x' => 0.150, 'y' => 1.974, 'z' => 0.074, ' ' => 25.000 }
+  end
+
   def self.english_score(bin)
     score = 0
-    words = bin.split ' '
-    common = %w(a an the this I he she they am is was were be and or)
-    words.each do |word|
-      score += 10 if common.include?(word) || common.include?(word.capitalize)
-      score += 5  if word =~ /^[a-z0-9\,\.\;\:\'\"\!\&\(\)\-\_]+$/
-      score -= 5  if word !~ /[aeiouy]/
-      score -= 10 if word.length > 10
+    letters = bin.split ''
+    english = english_letters
+    letters.each do |letter|
+      score += english.fetch(letter.downcase, 0)
+      score -= 10 unless letter =~ /[a-z0-9\,\.\;\:\'\"\!\&\(\)\-\_ ]/i
     end
     score
+  end
+
+  def self.hamming(str1, str2)
+    raise "Strings must be same length: '#{str1}' <=> '#{str2}'" unless str1.length == str2.length
+    str1.bytes.zip(str2.bytes).reduce(0) do |h, (a, b)|
+      h + (a^b).to_s(2).count("1")
+    end
   end
 end
--- cryptopals/message.rb
+++ cryptopals/message.rb
@@ -1,28 +1,29 @@
 require "#{__dir__}/crypto"
 
 class Message
-  def initialize(hex: nil, binary: nil)
+  def initialize(hex: nil, binary: nil, base64: nil)
     @hex = hex
     @binary = binary
+    @base64 = base64
   end
 
   def hex
-    @hex ||= Crypto.bin2hex(@binary)
+    @hex ||= Crypto.bin2hex(binary)
   end
 
   def binary
-    @binary ||= Crypto.hex2bin(@hex)
+    @binary ||= ( ( @hex && Crypto.hex2bin(@hex) ) || ( @base64 && Crypto.base642bin(@base64) ) )
   end
 
-  def to_base64
-    Crypto.bin2base64 binary
+  def base64
+    @base64 ||= Crypto.bin2base64 binary
   end
 
   def break_xor(possibles)
     possibles.map do |possible|
       decrypted = decrypt_xor possible
       { key: possible, message: decrypted, score: Crypto.english_score(decrypted) }
-    end.max { |val| val[:score] }
+    end.max_by { |val| val[:score] }
   end
 
   def decrypt_xor(key)
@@ -31,5 +32,14 @@ class Message
 
   def encrypt_xor(key)
     decrypt_xor key
+  end
+
+  def guess_keysize(n = 1)
+    (2..40).map do |ks|
+      slices = binary.scan(/.{#{ks}}/)[0, 4]
+      distances = (0..2).map { |i| Crypto.hamming(slices[i], slices[i+1]) / ( (ks-1) * 1.0 ) }
+      avg = distances.reduce(&:+) / ( slices.length * 1.0 )
+      { keysize: ks, avg: avg }
+    end.min_by(n) { |o| o[:avg] }
   end
 end
