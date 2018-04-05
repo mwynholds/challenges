779ba573109dd6ceea7ac375f61af625b6becbb3
Michael Wynholds <mike@carbonfive.com>
2018-04-05 16:56:44 -0700

    Set 2 Problem 1

---
+++ cryptopals/set2/problem1.rb
@@ -0,0 +1,14 @@
+require '../helper'
+
+class Problem1
+  def test
+  end
+
+  def solve
+    message = Message.new binary: "YELLOW SUBMARINE"
+    assert "YELLOW SUBMARINE000000000000000000000000000000000000000000000000", message.pad(16+48)
+    assert "YELLOW SUBMARINE\x04\x04\x04\x04", message.pad(20)
+  end
+end
+
+Helper.run Problem1
